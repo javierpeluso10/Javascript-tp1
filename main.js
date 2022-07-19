3fe9649429c690c423601fcae02bd5837fa436fb
Javier Peluso <jpelusocoderhouse@gmail.com>
2022-07-19 09:50:22 -0300

    tp1

---
+++ main.js
@@ -0,0 +1,29 @@
+
+    let inicio = prompt("Ingrese ESC para terminar, sino escriba EMPEZAR").toLowerCase()
+
+    if(inicio != 'esc' && inicio != 'empezar'){
+        alert('La palabra ingresada no existe, por favor introduzca "esc para salir" o "empezar para iniciar"')
+    }
+    while (inicio != "esc" && inicio == 'empezar') {
+
+        function sumarEdades(sumaUno, sumaDos, sumaTres) {
+            let suma = sumaUno + sumaDos + sumaTres
+            return suma
+        }
+    
+        function promedioDeEdad() {
+            let edadUno = parseFloat(prompt("Ingrese la primer edad para calcular el promedio"))
+            let edadDos = parseFloat(prompt("Ingrese la segunda edad para calcular el promedio"))
+            let edadTres = parseFloat(prompt("Ingrese la tercer edad para calcular el promedio"))
+    
+            let resultadoDeSuma = sumarEdades(edadUno, edadDos, edadTres)
+    
+            let promedio = resultadoDeSuma / 3
+            alert("El promedio de edad es " + promedio)
+        }
+        promedioDeEdad()
+        inicio = prompt("ingrese esc para terminar o CONTINUAR para seguir").toLowerCase()
+    }
+    
+
+
